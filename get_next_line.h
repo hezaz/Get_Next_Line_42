430acd2764e7c662354f07cb091020aebdc3ddfb
hezaz <62071557+hezaz@users.noreply.github.com>
2022-05-10 18:30:10 +0200

    vf

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,21 @@
-#ifndef	GNL_H
-	# define GNL_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
-#include <limits.h>
-#include <float.h>
-#include <locale.h>
-#include <errno.h>
-#include <stddef.h>
-#include <time.h>
-#include <assert.h>
-
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdint.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <math.h>
+# include <ctype.h>
+# include <limits.h>
+# include <float.h>
+# include <locale.h>
+# include <errno.h>
+# include <stddef.h>
+# include <time.h>
+# include <assert.h>
 
 size_t	ft_strlen(const char *s);
 char	*gnl_dup(char *s1, size_t n);
